fdf0d9aad9b276b28d173fd2f4280f49fab3214f
Matthew Mueller <mattmuelle@gmail.com>
2015-09-17 20:30:43 -0700

    add example

---
+++ example/index.js
@@ -0,0 +1,37 @@
+/**
+ * Module Dependencies
+ */
+
+var React = require('react')
+var Route = require('..')
+
+/**
+ * Example Component
+ */
+
+export default class Example extends React.Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <div className="Route">
+        <Route path='/'>
+          <h2>index</h2>
+        </Route>
+        <Route path='!/'>
+          <h2>index</h2>
+        </Route>
+        <Route path='/dashboard'>
+          <h2>dashboard</h2>
+        </Route>
+      </div>
+    )
+  }
+}
+
+if (typeof window !== 'undefined') {
+  var component = React.createElement(Example, {})
+  React.render(component, document.body)
+}
